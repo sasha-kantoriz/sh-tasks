952c1f5fbd41dd73d5806a11e5160c672bef0201
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2021-04-18 08:45:49 -0400

    Fix output

--- 01-backup.sh
+++ 01-backup.sh
@@ -6,7 +6,7 @@ mkdir -p $backup_folder
 date_prefix=`date +%Y-%m-%d_%H-%M-%S`
 
 for i in "$@"; do 
-	dest=$backup_folder/$date_prefix~`realpath $i | tr "/" "~"`
+	dest=$backup_folder/$date_prefix_`realpath $i | tr "/" "~"`
 	cp -R $i $dest
 	echo $dest
 done
