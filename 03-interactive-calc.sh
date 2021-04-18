8f2d4bd0b4a5db84eb13396edf57248de5cfa8a5
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2021-04-18 08:24:54 -0400

    Add file handling for interactive calc script

--- 03-interactive-calc.sh
+++ 03-interactive-calc.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-i=0
-while read x; do 
-	e="$i$x"
-       	i=$(( e ))
-done
+interactive_calc()
+{
+        while read x; do
+                e="$i$x"
+                i=$(( e ))
+        done
+}
 
-echo $i
+
+arg=${1:--}
+case $arg in
+	-)
+		i=0
+		interactive_calc
+		echo = $i
+		;;
+	*)
+		if [[ ! -f $1 ]]; then
+			exit 1
+		fi
+		lines=`cat $1 | sed -e 's/ //g'`
+		for l in $lines; do
+			e="$i$l"
+			i=$(( e ))
+		done
+		echo = $i
+		;;
+
+esac
+
 
